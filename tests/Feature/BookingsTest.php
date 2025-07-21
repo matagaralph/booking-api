6a7279879e6c87d3a3e7af8e5970dc648f13f51b
matagaralph <matagaralph@gmail.com>
2025-07-21 23:42:16 +0200

    refactor tests

--- tests/Feature/BookingsTest.php
+++ tests/Feature/BookingsTest.php
@@ -4,13 +4,13 @@ use App\Models\Role;
 use App\Models\User;
 
 test('user has access to bookings feature', function () {
-    $owner = User::factory()->create(['role_id' => Role::ROLE_USER]);
+    $owner = User::factory()->create()->assignRole(Role::ROLE_USER);
     $response = $this->actingAs($owner)->getJson('/api/v1/user/bookings');
     $response->assertStatus(200);
 });
 
 test('property owner does not have access to bookings feature', function () {
-    $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+    $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
     $response = $this->actingAs($owner)->getJson('/api/v1/user/bookings');
     $response->assertStatus(403);
 });
--- tests/Feature/PropertiesTest.php
+++ tests/Feature/PropertiesTest.php
@@ -4,13 +4,13 @@ use App\Models\Role;
 use App\Models\User;
 
 test('property owner has access to properties feature', function () {
-    $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+    $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
     $response = $this->actingAs($owner)->getJson('/api/v1/owner/properties');
     $response->assertStatus(200);
 });
 
 test('user does not have access to properties feature', function () {
-    $owner = User::factory()->create(['role_id' => Role::ROLE_USER]);
+    $owner = User::factory()->create()->assignRole(Role::ROLE_USER);
     $response = $this->actingAs($owner)->getJson('/api/v1/owner/properties');
     $response->assertStatus(403);
 });
