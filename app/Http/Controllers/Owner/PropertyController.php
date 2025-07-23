ed863ff45ffd3c6bf13212b94b844d0af6381073
matagaralph <matagaralph@gmail.com>
2025-07-23 17:36:57 +0200

    build registering properties

--- app/Http/Controllers/Owner/PropertyController.php
+++ app/Http/Controllers/Owner/PropertyController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePropertyRequest;
+use App\Models\Property;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 class PropertyController extends Controller {
     use AuthorizesRequests;
@@ -11,7 +14,12 @@ class PropertyController extends Controller {
     public function index() {
         $this->authorize('properties-manage');
 
-        // Will implement property management later
-        return response()->json(['success' => true]);
+        return response()->json([]);
+    }
+
+    public function store(StorePropertyRequest $request) {
+        $this->authorize('properties-manage');
+
+        return Property::create($request->validated());
     }
 }
